83ce2dfd8643f786bab9ae24cb45903c604c1f8a
ShreyaShuklaa <50106221+ShreyaShuklaa@users.noreply.github.com>
2019-06-25 20:50:22 +0530

    Add files via upload

---
+++ calculation.js
@@ -0,0 +1,13 @@
+exports.print1 =function(a)
+{
+	return parseInt(a);
+}
+exports.print2 =function(b)
+{
+	return  parseInt(b);
+}
+
+exports.emi= function(c,d)
+{
+	return ((0.8)*parseInt(c)*parseInt(d));
+}
